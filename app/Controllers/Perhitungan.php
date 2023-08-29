7108cb28c2f537948b49e55db8991b54da16198f
Wildania Estiyanti <76966965+daniaa17@users.noreply.github.com>
2023-08-29 11:37:41 +0700

    fc

--- app/Controllers/Perhitungan.php
+++ app/Controllers/Perhitungan.php
@@ -7,49 +7,74 @@ use App\Models\BasisPengetahuan;
 
 class Perhitungan extends BaseController
 {
-
-    protected $pengetahuan;
-
-    function __construct()
+    public function index()
     {
-        $this->pengetahuan = new BasisPengetahuan();
+        $kriteriaModel = new KriteriaProduk();
+        $kriteria = $kriteriaModel->findAll(); // Mengambil data kriteria
+
+        return view('penentuan_kemasan', ['kriteria' => $kriteria]);
     }
 
-    public function pencarian() {
-        $selectedCriteria = $this->request->getPost('kriteria');
-        $matchedRules = $this->pengetahuan->getMatchedRules($selectedCriteria);
-    
-        $bestMatch = null;
-        
+    public function pencarian()
+    {
+        $selectedCriteria = $this->request->getVar('kriteria'); // Ambil data kriteria dari form
+
+        if (!$selectedCriteria) {
+            return redirect()->to('/pencarian_kemasan'); // Redirect kembali jika tidak ada kriteria terpilih
+        }
+
+        $basisModel = new BasisPengetahuan();
+        $matchedRules = $basisModel->getMatchedRules($selectedCriteria);
+
+        $result = []; // Simpan hasil perhitungan berdasarkan matched rules
         foreach ($matchedRules as $rule) {
-            $ruleCriteria = explode(',', $rule->kriteria_produk_id);
-            $match = true;
-    
-            foreach ($selectedCriteria as $criteria) {
-                if (!in_array($criteria, $ruleCriteria)) {
-                    $match = false;
-                    break;
-                }
-            }
-    
-            if ($match) {
-                $bestMatch = $rule;
-                break; 
+            $jenisKemasan = $basisModel->getJenisKemasanName($rule->jenis_kemasan_id);
+            if ($jenisKemasan) {
+                $result[] = $jenisKemasan;
             }
         }
-    
-        $jenisKemasanName = null;
-        
-        if ($bestMatch) {
-            $jenisKemasanId = $bestMatch->jenis_kemasan_id;
-            $jenisKemasanName = $this->pengetahuan->getJenisKemasanName($jenisKemasanId);
-        }
-    
-        return view('user/hasil-pencarian', ['jenisKemasanName' => $jenisKemasanName]);
+
+        // Tampilkan hasil perhitungan
+        return view('hasil_perhitungan', ['result' => $result]);
     }
+
+    // protected $pengetahuan;
+
+    // function __construct()
+    // {
+    //     $this->pengetahuan = new BasisPengetahuan();
+    // }
+
+    // public function pencarian() {
+    //     $selectedCriteria = $this->request->getPost('kriteria');
+    //     $matchedRules = $this->pengetahuan->getMatchedRules($selectedCriteria);
     
+    //     $bestMatch = null;
+        
+    //     foreach ($matchedRules as $rule) {
+    //         $ruleCriteria = explode(',', $rule->kriteria_produk_id);
+    //         $match = true;
     
+    //         foreach ($selectedCriteria as $criteria) {
+    //             if (!in_array($criteria, $ruleCriteria)) {
+    //                 $match = false;
+    //                 break;
+    //             }
+    //         }
     
+    //         if ($match) {
+    //             $bestMatch = $rule;
+    //             break; 
+    //         }
+    //     }
     
+    //     $jenisKemasanName = null;
+        
+    //     if ($bestMatch) {
+    //         $jenisKemasanId = $bestMatch->jenis_kemasan_id;
+    //         $jenisKemasanName = $this->pengetahuan->getJenisKemasanName($jenisKemasanId);
+    //     }
     
+    //     return view('user/hasil-pencarian', ['jenisKemasanName' => $jenisKemasanName]);
+    // }   
 }
